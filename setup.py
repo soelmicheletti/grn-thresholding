5233f2d5189662b9781793525dde7f4947272841
Soel Micheletti <soel.micheletti@hotmail.it>
2023-03-04 09:18:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "statistics",
         "causality",
     ],
-    install_requires=["mixem>=0.1.4"],
+    install_requires=["mixem>=0.1.4", "sklearn>=0.15.2"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
